3192b1e9b1a5c984643a21557654242bb060ca69
VincentLaf <zsjperiod@foxmail.com>
2018-01-07 23:12:18 +0800

    修改Glide版本

--- app/src/main/java/com/vincentlaf/androidarsenal/MainActivity.java
+++ app/src/main/java/com/vincentlaf/androidarsenal/MainActivity.java
@@ -1,15 +1,12 @@
 package com.vincentlaf.androidarsenal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.transition.TransitionManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void loadImage(View view) {
         String url = "https://cn.bing.com/az/hprichbg/rb/StelvioPass_ZH-CN13895715460_1920x1080.jpg";
-        Glide.with(this).load(url).into(imageView);
+        //可以接收Activity、Fragment、ApplicationContext
+        //传入Activity或者Fragment，Activity或Fragment被销毁的时候，图片加载也会停止。
+        //传入的是ApplicationContext，那么只有当应用程序被杀掉的时候，图片加载才会停止。
+        Glide.with(this)
+                .load(url)
+                .placeholder(R.drawable.landscapes_cherry)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .error(R.drawable.error)
+                .into(imageView);
     }
 }
